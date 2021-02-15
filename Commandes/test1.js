142acd436c3f97d711a4c0d6d35d80136c6941e6
Valdouz <voxfy.dev@gmail.com>
2021-02-15 14:58:04 +0100

    aaaa

---
+++ Commandes/test1.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const {PREFIX} = require("../config.js");
+
+module.exports.run = (client, message, args) => {
+
+
+console.log(db.get(`xp_${message.guild.id}.${id}.lvl`))
+
+}
+
+module.exports.help = {
+    name: 'test',
+  };
