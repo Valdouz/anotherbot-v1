6731eccc1943c9571a778e77d17363ea79f964ff
Voxfy <Voxfy.dev@gmail.com>
2021-02-17 12:09:32 +0100

    Add files via upload

--- Events/ready.js
+++ Events/ready.js
@@ -1,6 +1,11 @@
-module.exports = async(client, data) => {
-    client.user.setActivity(`.help`, {
-        type: "STREAMING",
-        url: "https://www.twitch.tv/valdouz_"
-      });
-}
+module.exports = async(client) => {
+    let status_liste = [
+        `.help`,
+        `sur ${client.guilds.cache.size} serveurs`
+      ]
+    setInterval(function() {
+    let st = status_liste[Math.floor(Math.random() * status_liste.length )]
+    client.user.setActivity(st, { type: 'STREAMING', url: "https://www.twitch.tv/valdouz_" })
+  
+    }, 50000)
+};
