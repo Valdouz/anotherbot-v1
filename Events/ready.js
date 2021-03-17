f2388fd15a3b8d49e763bc980f0c182fc4e633e3
Voxfy <Voxfy.dev@gmail.com>
2021-03-17 15:50:38 +0100

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,11 +1,13 @@
+const { Message } = require("discord.js");
+const config = require("../config.js");
 module.exports = async(client) => {
     let status_liste = [
         `.help`,
-        `{client.guilds.cache.size} serveurs`
+        `${client.guilds.cache.size} serveurs`
       ]
     setInterval(function() {
     let st = status_liste[Math.floor(Math.random() * status_liste.length )]
     client.user.setActivity(st, { type: 'STREAMING', url: "https://www.twitch.tv/valdouz_" })
-  client.channels.cache.get("810281784846712913").send(`**${client.user.tag} viens de démarrer !** :white_check_mark:`);
+    client.channels.cache.get(config.STARTCHANNEL).send(`:white_check_mark: | ${client.user.tag} viens de démarrer !`);
     }, 50000)
 };
