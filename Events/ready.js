c42baa407297eee33a737907a29e0111fa4bd37d
Voxfy <Voxfy.dev@gmail.com>
2021-03-17 15:26:39 +0100

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -6,6 +6,6 @@ module.exports = async(client) => {
     setInterval(function() {
     let st = status_liste[Math.floor(Math.random() * status_liste.length )]
     client.user.setActivity(st, { type: 'STREAMING', url: "https://www.twitch.tv/valdouz_" })
-  
+  client.channels.cache.get("810281784846712913").send(`**${client.user.tag} viens de démarrer !** :white_check_mark:`);
     }, 50000)
 };
