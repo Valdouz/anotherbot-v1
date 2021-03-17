e4cbe06a693d32da29fffd64d8e35d003d14b1b0
Voxfy <Voxfy.dev@gmail.com>
2021-03-17 15:15:55 +0100

    typo

--- Commandes/userinfo.js
+++ Commandes/userinfo.js
@@ -21,7 +21,7 @@ exports.run = (client, message, args) => {
         const member = message.guild.member(user);
         const moment = require('moment');
         
-        if(user.bot === 'true') {
+        if(user.bot === true) {
             robot = 'Oui'
         } else {
             robot = 'Non'
