c8f5fe5d2a96526988cfb86a5b6b0d2dc279c088
Valdouz <voxfy.dev@gmail.com>
2021-03-28 21:57:36 +0200

    add exemple command

---
+++ exemple_commands.js
@@ -0,0 +1,14 @@
+//const
+const Discord = require("discord.js");
+
+//structure de command handler
+module.exports.run = (client, message, args) => {
+
+//fin de la structure 
+}
+
+//exportation de la commande pour le nom (et l'ancien help)
+module.exports.help = {
+    name: '',
+    alias: [""],
+}
