e0fccc550bd903144722a4b8c0691f71e53e4121
AkiraDinosaurus <voxfy@skypvp.fr>
2021-05-10 23:57:18 +0200

    Modification de la commande recharge.js Fix final de la commande (celle-ci marche enfin)

--- Commandes/recharge.js
+++ Commandes/recharge.js
@@ -1,13 +1,14 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(client, message, args) => {
-    if(message.author.id !== "748103597098795059") return message.channel.send(":x: **Vous n'avez pas les permissions suffisantes pour exécuter cette commande !**")
+    if(message.author.id !== "748103597098795059" && message.author.id !== "563726267246182400") return message.channel.send(":x: **Vous n'avez pas les permissions suffisantes pour exécuter cette commande !**")
     if(!args[0]) return message.channel.send(":x: **Vous devez fournir la commande à recharger !**\n`.recharge <nom de la commande>`")
     let msg = await message.channel.send("Rechargement de la commande " + args[0] + "...")
     try {
         let commande = client.commands.get(args[0])
+	if(!commande) return msg.edit(":x: **La commande n'existe pas !**")
         client.commands.delete(commande.help.name)
-        delete require.cache[require.resolve(`./${args[0]}.js`  )] 
+        delete require.cache[require.resolve(`./${args[0]}.js`)]
         console.log(`commande ${commande.help.name + ".js"} déchargé !`)
         if(commande.help.alias) {
                 if(commande.help.alias.length > 1) {
@@ -22,7 +23,7 @@ module.exports.run = async(client, message, args) => {
             }
         }
         let ncommande = require("./" + args[0] + ".js")
-        client.commands.set(ncommande.help.name, commande)
+        client.commands.set(ncommande.help.name, ncommande)
         console.log(`commande ${ncommande.help.name + ".js"} chargé !`)
         if(ncommande.help.alias) {
                 if(ncommande.help.alias.length > 1) {
@@ -32,14 +33,14 @@ module.exports.run = async(client, message, args) => {
                 })
             }
             else {
-                client.commands.set(ncommande.help.alias[0], commande)
+                client.commands.set(ncommande.help.alias[0], ncommande)
                 console.log(`| --> alias ${ncommande.help.alias} de la commande ${ncommande.help.name} chargé !`)
             }
         }
         msg.edit(":white_check_mark: **Commande** " + args[0] + " **rechargé !**")
     }
     catch(err) {
-        msg.edit(":x: **Erreur lors du rechargement de la commande : ```xl\n" + err + "\n```")
+        msg.edit(":x: **Erreur lors du rechargement de la commande :** ```xl\n" + err.stack + "\n```")
     }
 };
 
