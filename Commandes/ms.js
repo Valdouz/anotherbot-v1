62e45f0648bb46831e9f4bf05cffa030b48c2148
AkiraDinosaurus <voxfy@skypvp.fr>
2021-05-10 23:58:16 +0200

    Modification de la commande ms.js Patch de certains bugs

--- Commandes/ms.js
+++ Commandes/ms.js
@@ -1,3 +1,4 @@
+
 const Discord = require("discord.js");
 const {PREFIX} = require("../config.js");
 const { MessageEmbed } = require('discord.js');
@@ -6,7 +7,7 @@ module.exports.run = (client, message, args) => {
 
 const ping = new MessageEmbed()
 
-.setTitle(`⏱️Pinging\.\.\.`)
+.setTitle(`() Ping en cours...`)
 .setColor('#c215e9')
 .setDescription(`**:ping_pong: Pong!\nLa latence est de : \`${Math.round(client.ws.ping)}ms\`. **`)
 .setThumbnail(message.author.avatarURL())
@@ -18,7 +19,7 @@ message.channel.send(ping);
 
 
 module.exports.help = {
-  name:  "Ms",
+  name:  "ms",
   alias: ["ms"],
   category: 'informations',
   utility: 'permet d\'afficher la latence du bot'
